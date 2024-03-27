c959623daa3612fa754e64732a50a6a3af99a1d8
dadad <dadado20029@gmail.com>
2024-03-27 08:36:14 +0200

    #20: Change the error handle for 403 Forbidden and 401 Unauthorized.

---
+++ src/main/java/com/example/springweb/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,36 @@
+package com.example.springweb.security;
+
+import com.example.springweb.exceptions.ApiError;
+import com.example.springweb.exceptions.ApiErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException arg2)
+            throws IOException, ServletException {
+        ApiError apiError = ApiError.builder()
+                .message(ApiErrorCode.FORBIDDEN.getMessage())
+                .debugMessage(arg2.getMessage())
+                .code(ApiErrorCode.FORBIDDEN.name())
+                .build();
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        OutputStream responseStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(responseStream, apiError);
+        responseStream.flush();
+    }
+}
