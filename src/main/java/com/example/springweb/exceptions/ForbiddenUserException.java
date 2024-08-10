ccccbb526f9c193e829410fb9de5a9a64a067a68
dadad <dadado20029@gmail.com>
2024-08-10 19:54:12 +0300

    #34: add static code analysis

--- src/main/java/com/example/springweb/exceptions/ForbiddenUserException.java
+++ src/main/java/com/example/springweb/exceptions/ForbiddenUserException.java
@@ -1,6 +1,6 @@
 package com.example.springweb.exceptions;
 
-public class ForbiddenUserException extends RuntimeException{
+public class ForbiddenUserException extends RuntimeException {
 
     public ForbiddenUserException(String message) {
         super(message);
--- src/main/java/com/example/springweb/repository/ProductRepository.java
+++ src/main/java/com/example/springweb/repository/ProductRepository.java
@@ -1,12 +1,8 @@
 package com.example.springweb.repository;
 
 import com.example.springweb.entity.Product;
-import com.example.springweb.entity.User;
 import com.example.springweb.exceptions.ProductNotFoundException;
-import com.example.springweb.exceptions.UserAppointmentNotFoundException;
-import com.example.springweb.exceptions.UserNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.parameters.P;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
