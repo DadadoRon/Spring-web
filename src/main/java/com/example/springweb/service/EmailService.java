364c89ef8fd0fac1e816c4f6a284a3f193ea23af
dadad <dadado20029@gmail.com>
2024-08-23 00:17:59 +0300

    #46: Add password reset feature

--- src/main/java/com/example/springweb/service/EmailService.java
+++ src/main/java/com/example/springweb/service/EmailService.java
@@ -1,5 +1,7 @@
 package com.example.springweb.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.mail.*;
@@ -12,11 +14,14 @@ public class EmailService {
     private final   String to = "springweb9@gmail.com";
     private final String from = "springweb9@gmail.com";
     private final String password = "fjhu gilo rxod qjpz";
-    private final String host = "smtp.gmail.com";
-    private final String smtpPort = "465";
+
+
+    private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
 
 
     public void sendPasswordResetEmail(String to, String resetLink) {
+        final String host = "smtp.gmail.com";
+        final String smtpPort = "465";
         Properties properties = new Properties();
         properties.put("mail.smtp.host", host);
         properties.put("mail.smtp.port", smtpPort);
@@ -24,6 +29,7 @@ public class EmailService {
         properties.put("mail.smtp.auth", "true");
 
         Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(from, password);
             }
@@ -34,12 +40,12 @@ public class EmailService {
             MimeMessage message = new MimeMessage(session);
             message.setFrom(new InternetAddress(from));
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-            message.setSubject("Password Reset Request");
+            message.setSubject("Password reset request");
             message.setText("Click the following link to reset your password: " + resetLink);
 
             Transport.send(message);
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Failed to send password reset email to {}", to, e);
         }
     }
 }
