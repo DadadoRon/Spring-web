ad727b69b8837ce993e3f2864916a5793a53c065
dadad <dadado20029@gmail.com>
2024-10-17 19:51:24 +0300

    #63: Combine RestResponseEntityGlobalExceptionHandler into GlobalExceptionHandler

--- src/main/java/com/example/springweb/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/springweb/exceptions/handler/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.example.springweb.exceptions.handler;
 
-import com.example.springweb.exceptions.ApiError;
-import com.example.springweb.exceptions.ApiErrorCode;
+import com.example.springweb.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -15,7 +14,7 @@ import java.util.List;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+    public ResponseEntity<ApiError> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
         List<String> errors = new ArrayList<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String errorMessage = error.getDefaultMessage();
@@ -27,5 +26,65 @@ public class GlobalExceptionHandler {
                 .code(ApiErrorCode.VALIDATION_ERROR.name())
                 .build();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+    }
+
+    @ExceptionHandler(value = {EntityNotFoundException.class})
+    public ResponseEntity<ApiError> handleUserNotFoundException(EntityNotFoundException ex) {
+        ApiError apiError = ApiError.builder()
+                .message(ex.getCode().getMessage())
+                .debugMessage(ex.getMessage())
+                .code(ex.getCode().name())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
+    }
+
+    @ExceptionHandler(value = {ForbiddenUserException.class})
+    public ResponseEntity<ApiError> handleAccessDeniedException(ForbiddenUserException ex) {
+        ApiError apiError = ApiError.builder()
+                .message(ApiErrorCode.FORBIDDEN.getMessage())
+                .debugMessage(ex.getMessage())
+                .code(ApiErrorCode.FORBIDDEN.name())
+                .build();
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(apiError);
+    }
+
+    @ExceptionHandler(value = {InvalidPasswordException.class})
+    public ResponseEntity<ApiError> handleAccessDeniedException(InvalidPasswordException ex) {
+        ApiError apiError = ApiError.builder()
+                .message(ApiErrorCode.INVALID_PASSWORD.getMessage())
+                .debugMessage(ex.getMessage())
+                .code(ApiErrorCode.INVALID_PASSWORD.name())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+    }
+
+    @ExceptionHandler(value = {TokenNotFoundException.class})
+    public ResponseEntity<ApiError> handleTokenNotFoundException(TokenNotFoundException ex) {
+        ApiError apiError = ApiError.builder()
+                .message(ApiErrorCode.TOKEN_NOT_FOUND.getMessage())
+                .debugMessage(ex.getMessage())
+                .code(ApiErrorCode.TOKEN_NOT_FOUND.name())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
+    }
+
+    @ExceptionHandler(value = {InvalidTokenException.class})
+    public ResponseEntity<ApiError> handleInvalidTokenException(InvalidTokenException ex) {
+        ApiError apiError = ApiError.builder()
+                .message(ApiErrorCode.INVALID_TOKEN.getMessage())
+                .debugMessage(ex.getMessage())
+                .code(ApiErrorCode.INVALID_TOKEN.name())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+    }
+
+    @ExceptionHandler(value = {ExternalServiceException.class})
+    public ResponseEntity<ApiError> handleWeatherDataNotFoundException(ExternalServiceException ex) {
+        ApiError apiError = ApiError.builder()
+                .message(ApiErrorCode.WEATHER_DATA_NOT_FOUND.getMessage())
+                .debugMessage(ex.getMessage())
+                .code(ApiErrorCode.WEATHER_DATA_NOT_FOUND.name())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
     }
 }
